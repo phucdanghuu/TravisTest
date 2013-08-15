1f933339d6bc9b812948a6a9e424c8e5236eab10
phucdanghuu <huuphuc.cntt@gmail.com>
2013-08-15 11:02:13 +0700

    test5

--- buildstuff.sh
+++ buildstuff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FAILED=0
-cd ./FridgeMagnet
-ant debug
-if [ "$?" = 1 ]; then
-    echo "FridgeMagnet build failed!"
-    FAILED=1
-fi
-cd ..
+# cd ./FridgeMagnet
+# ant debug
+# if [ "$?" = 1 ]; then
+    # echo "FridgeMagnet build failed!"
+    # FAILED=1
+# fi
+# cd ..
 
 cd ./FridgeMagnetTest
 ant clean test
